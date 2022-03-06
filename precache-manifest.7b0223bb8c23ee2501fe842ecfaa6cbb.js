d0e3eae8d26b35c915aaff8e213480c568015fe2
Jaron <j-bloom7@protonmail.com>
2022-03-05 23:10:46 -0500

    Updates

---
+++ precache-manifest.7b0223bb8c23ee2501fe842ecfaa6cbb.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "5170ef428b08116af41d",
+    "url": "/static/css/main.f6fc85e0.chunk.css"
+  },
+  {
+    "revision": "5170ef428b08116af41d",
+    "url": "/static/js/main.5170ef42.chunk.js"
+  },
+  {
+    "revision": "a3ee8d29406c8308c8d5",
+    "url": "/static/css/1.ab338901.chunk.css"
+  },
+  {
+    "revision": "a3ee8d29406c8308c8d5",
+    "url": "/static/js/1.a3ee8d29.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "cdef20c49404069a04a295ed9d0d4fd1",
+    "url": "/static/media/technologies.cdef20c4.jpg"
+  },
+  {
+    "revision": "2c5d0ebbdec44e773d8487f7188057d9",
+    "url": "/static/media/portfolio-photo.2c5d0ebb.jpg"
+  },
+  {
+    "revision": "578a537887983c65ef97b2be843273ff",
+    "url": "/static/media/portfoliophoto-placeholder.578a5378.jpg"
+  },
+  {
+    "revision": "2fac58c73852d2a45673587003f57cda",
+    "url": "/index.html"
+  }
+];
